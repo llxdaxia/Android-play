85d3adff487b0c437a86f446826532c8f658ae26
llxal <18983679028@163.com>
2015-12-07 11:08:41 +0800

    上课中

--- app/src/main/java/cn/hotwoo/play/MainActivity.java
+++ app/src/main/java/cn/hotwoo/play/MainActivity.java
@@ -18,8 +18,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 startActivity(intent);
-
-
             }
         });
     }
